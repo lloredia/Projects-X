0f0b65e80323eb44731d9ca5cbb80633d9b547be
Robin Hood~ <53010259+lloredia@users.noreply.github.com>
2019-08-08 15:48:38 -0400

    Delete install_polychrom.sh

--- install_polychrom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "deb http://ppa.launchpad.net/polychromatic/stable/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/polychromatic.list
-apt-key adv --recv-key --keyserver keyserver.ubuntu.com 96B9CD7C22E2C8C5
-apt update
-apt install polychromatic
