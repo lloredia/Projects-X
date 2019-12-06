a76d5c2d960e646878ab76b70a654db452ae743c
An=onymous881453 <53010259+lloredia@users.noreply.github.com>
2019-12-05 19:14:45 -0600

    Update StartPythonWebServer.sh

--- devops/StartPythonWebServer.sh
+++ devops/StartPythonWebServer.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
- nohup python -m SimpleHTTPServer >/dev/null 2>&1 &
-~
+echo -ne '#####                     (33%)\r'
+sleep 2
+echo -ne '#############             (66%)\r'
+sleep 2
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+sleep 1
+
+nohup python -m SimpleHTTPServer >/dev/null 2>&1 &
+
+sleep 2
+
+echo -e "[+] Python Web-Server Is Up and Running... "
